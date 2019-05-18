bb0900987f7bb5008fbb2e02f8f92d2b1e60d3bf
kareem sameer <k.harazeen@gmail.com>
2019-05-18 12:34:53 +0300

    add category and latest pages

--- resources/views/pages/common/photo_block.blade.php
+++ resources/views/pages/common/photo_block.blade.php
@@ -2,11 +2,13 @@
    <span class="hover-content">
       <h5 class="text-overflow title-hover-content" title="Heart Letter">
          {{$photo->title}}
-      </h5> 
+      </h5>
+      @if($photo->user)
       <h5 class="text-overflow author-label mg-bottom-xs" title="{{$photo->user->name}}">
          <!-- <img src="public/avatar/purenetwork-114984932140w6vdcdjkl.png" alt="User" class="img-circle" style="width: 20px; height: 20px; display: inline-block; margin-right: 5px;"> -->
          <em>{{$photo->user->name}}</em>
       </h5> 
+      @endif
       <span class="timeAgo btn-block date-color text-overflow" data="clipart-pink-heart.html"></span>
       <span class="sub-hover">
       <span class="myicon-right"><i class="fa fa-line-chart myicon-right"></i> 130</span>
