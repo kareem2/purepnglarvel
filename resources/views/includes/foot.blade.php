33ccabbec80cf2aea2e0c241eaee2e4d984a90a0
kareem sameer <k.harazeen@gmail.com>
2019-05-24 18:44:48 +0300

    remove facebook ads.

--- resources/views/includes/foot.blade.php
+++ resources/views/includes/foot.blade.php
@@ -42,14 +42,6 @@
     </script>
 <script src="{{ URL::asset('/plugins/iCheck/icheck.min.js') }}" type="text/javascript"></script>
 <script type="text/javascript">
-
-    (function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = '../../../connect.facebook.net/de_DE/sdk.js#xfbml=1&version=v2.12';
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
     
 $('#imagesFlex').flexImages();
 
@@ -60,68 +52,9 @@ $('#btnFormPP').click(function(e){
 $('input').iCheck({
           radioClass: 'iradio_flat-green',
           checkboxClass: 'icheckbox_square-green',
-        });
+});
 
 
-         
-  
-  //<<<---------- Comments Likes
-$(document).on('click','.comments-likes',function() {
-    element  = $(this);
-    var id   = element.attr("data-id");
-    var info = 'comment_id=' + id;
-
-        element.removeClass('comments-likes');
-
-        $.ajax({
-            headers: {
-            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-            },
-           type: "POST",
-           url: "https://purepng.com/comments/likes",
-           data: info,
-           success: function( data ) {
-
-
-                $( '#collapse'+ id ).html(data);
-                $('[data-toggle="tooltip"]').tooltip();
-
-                if( data == '' ){
-                    $( '#collapse'+ id ).html("Error occurred");
-                }
-                }//<-- $data
-            });
-   });
-
-  
-    //<<---- PAGINATION AJAX
-        // $(document).on('click','.pagination a', function(e){
-        //     e.preventDefault();
-        //     var page = $(this).attr('href').split('page=')[1];
-        //     $.ajax({
-        //         headers: {
-        //     'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-        //     },
-        //             url: '//purepng.com/ajax/comments?photo=30610&page=' + page
-
-
-        //     }).done(function(data){
-        //         if( data ) {
-
-        //             scrollElement('#gridComments');
-
-        //             $('.gridComments').html(data);
-
-        //             jQuery(".timeAgo").timeago();
-
-        //             $('[data-toggle="tooltip"]').tooltip();
-        //         } else {
-        //             sweetAlert("Oops...", "Error occurred", "error");
-        //         }
-        //         //<**** - Tooltip
-        //     });
-
-        // });//<<---- PAGINATION AJAX
 </script>
 <script type="application/ld+json">
 {
@@ -144,17 +77,17 @@ $(document).on('click','.comments-likes',function() {
 
 <script type="text/javascript">
 
-     //$('#imagesFlex').flexImages({ rowHeight: 220, maxRows: 8, truncate: true });
+   //$('#imagesFlex').flexImages({ rowHeight: 220, maxRows: 8, truncate: true });
 
 
-    jQuery(document).ready(function( $ ) {
-      $('.counter').counterUp({
-      delay: 10, // the delay time in ms
-      time: 1000 // the speed time in ms
-      });
+  jQuery(document).ready(function( $ ) {
+    $('.counter').counterUp({
+    delay: 10, // the delay time in ms
+    time: 1000 // the speed time in ms
     });
+  });
 
+   
      
-       
-    </script>
+  </script>
 This page took <strong>{{ (microtime(true) - LARAVEL_START) }} </strong> seconds to render  
